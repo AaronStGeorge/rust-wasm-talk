c7dda4eaa14591aa0de127378a20319c4e4710e5
AaronStGeorge <aaronstgeorge@gmail.com>
2018-04-01 13:44:52 -0600

    Update to new parser API

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ extern crate catlox;
 
 use wasm_bindgen::prelude::*;
 
+use catlox::interpreter::*;
 use catlox::lexer::*;
 use catlox::parser::*;
-use catlox::interpreter::*;
 use catlox::resolver::*;
 
 #[wasm_bindgen(module = "./index")] // what ES6 module to import from
@@ -20,6 +20,7 @@ extern "C" {
 #[wasm_bindgen]
 pub struct LoxInterpreter {
     interpreter: Interpreter,
+    parse_seed: usize,
 }
 
 #[wasm_bindgen]
@@ -27,18 +28,21 @@ impl LoxInterpreter {
     pub fn new() -> LoxInterpreter {
         LoxInterpreter {
             interpreter: Interpreter::new(Box::new(|s| out(s))),
+            parse_seed: 0,
         }
     }
 
     pub fn run(&mut self, s: &str) {
         let tokens: Vec<Token> = Lexer::new(s).collect();
 
-        match Parser::new(&tokens).parse() {
-            Ok(statements) => match resolve(&statements, &mut self.interpreter) {
+        match Parser::new(&tokens, self.parse_seed).parse() {
+            Ok((new_parse_seed, statements)) => match resolve(&statements, &mut self.interpreter) {
                 Ok(()) => match self.interpreter.interpret(&statements) {
-                    Ok(()) => (),
+                    Ok(()) => {
+                        self.parse_seed = new_parse_seed;
+                    }
                     Err(error) => err(&format!("Interpreter Error: {}", error)),
-                }
+                },
                 Err(error) => err(&format!("Resolver Error: {}", error)),
             },
             Err(error) => err(&format!("Parse Error: {}", error)),
